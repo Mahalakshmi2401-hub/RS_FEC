8824fabd137c7241edacbdc3d68f7e8a321482c2
Mahalakshmi2401-hub <Mahalakshmi.R@siliconpatterns.com>
2026-09-16 14:28:40 +0530

    Update rs_fec_counters.v

--- rs_fec_counters.v
+++ rs_fec_counters.v
@@ -1,75 +1,42 @@
-//=====================================================
-// PAYLOAD / PARITY COUNTERS
-//=====================================================
-
+`timescale 1ns/1ps
 module rs_fec_counters
 (
     input  wire       clk,
     input  wire       rst_n,
-
     input  wire [7:0] payload_bytes,
     input  wire [4:0] parity_bytes,
-
     input  wire       payload_cnt_en,
     input  wire       parity_cnt_en,
-
     input  wire       cnt_clear,
-
     output wire       payload_done,
     output wire       parity_done,
-
-    output reg        [4:0] parity_cnt
+    output reg  [4:0] parity_cnt
 );
-     reg              [7:0] payload_cnt;
-//-----------------------------------------------------
-// Done Detection
-//-----------------------------------------------------
+reg [7:0] payload_cnt;
 
-assign payload_done =
-       payload_cnt_en &&
-       (payload_cnt == payload_bytes-1);
+assign payload_done = payload_cnt_en && (payload_cnt == (payload_bytes - 8'd1));
 
-assign parity_done =
-       parity_cnt_en &&
-       (parity_cnt == parity_bytes-1);
+assign parity_done = parity_cnt_en && (parity_cnt == (parity_bytes - 5'd1));
 
-//-----------------------------------------------------
-// Payload Counter
-//-----------------------------------------------------
-
-always @(posedge clk or negedge rst_n)
-begin
+always @(posedge clk or negedge rst_n) begin
     if(!rst_n)
         payload_cnt <= 8'd0;
-
     else if(cnt_clear)
         payload_cnt <= 8'd0;
-
     else if(payload_done)
         payload_cnt <= 8'd0;
-
     else if(payload_cnt_en)
         payload_cnt <= payload_cnt + 8'd1;
 end
 
-//-----------------------------------------------------
-// Parity Counter
-//-----------------------------------------------------
-
-always @(posedge clk or negedge rst_n)
-begin
+always @(posedge clk or negedge rst_n) begin
     if(!rst_n)
         parity_cnt <= 5'd0;
-
     else if(cnt_clear)
         parity_cnt <= 5'd0;
-
     else if(parity_done)
         parity_cnt <= 5'd0;
-
     else if(parity_cnt_en)
         parity_cnt <= parity_cnt + 5'd1;
 end
-
-
 endmodule
